0eba68a94ee7d34fd0f32f14ec34ca0013c1a685
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2024-10-11 12:25:47 +0800

    攻击2推箱子

--- Test/LevelTest/box.gd
+++ Test/LevelTest/box.gd
@@ -1,14 +1,18 @@
 extends StaticBody2D
 
-
-# Called when the node enters the scene tree for the first time.
+var player:CharacterBody2D
+var hand:AnimatedSprite2D
 func _ready() -> void:
-	pass # Replace with function body.
+	player=get_tree().get_first_node_in_group("player")
+	hand=player.get_node("Body/Hand")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if $Left.is_colliding() && $Left.get_collider().name=="Player":
-		self.position.x+=-2
+		if hand.animation=="Atack2" && hand.frame==2:
+			self.position.x+=38
+
 	if $Right.is_colliding() && $Right.get_collider().name=="Player":
-		self.position.x+=2
+		if hand.animation=="Atack2" && hand.frame==2:
+			self.position.x+=-38
