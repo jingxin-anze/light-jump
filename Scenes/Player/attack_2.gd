051e6c86f767bb6fc28ec70511b362fd8e889f00
aidewn <50902442+aidewn@users.noreply.github.com>
2024-10-14 00:25:51 +0800

    修改直接跳蛤蟆身上落下报错的bug

--- Scenes/Player/attack_2.gd
+++ Scenes/Player/attack_2.gd
@@ -11,7 +11,17 @@ func physics_process_update(delta: float) -> void:
 	player.player_move_1(player.fall_gravity,delta)
 	if not player.is_on_floor():
 		state_machine.change_state("Fall")
+		
+	check_frog_collision(player, player.right, 40)
+	check_frog_collision(player, player.left, -40)
 
 ## 退出状态
 func exit() -> void:
 	pass
+	
+	
+func check_frog_collision(player: Player, ray: RayCast2D, offset: float) -> void:
+	if ray.is_colliding() and ray.get_collider().name == "Frog":
+		if player.hand.animation == "Attack2" and player.hand.frame == 2:
+			var frog = ray.get_collider()
+			frog.position.x += offset
