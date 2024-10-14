c9a20ca900794c9a5f310747beb656aa4181b15c
aidewn <50902442+aidewn@users.noreply.github.com>
2024-10-14 22:05:10 +0800

    跳跃逻辑补充

--- Scenes/Module/Snake/Script/jump.gd
+++ Scenes/Module/Snake/Script/jump.gd
@@ -2,12 +2,19 @@ extends StateBase
 
 var player: Player 
 @onready var detect_the_player: RayCast2D = $"../../DetectThePlayer"
+@export var snake: Snake
+
+
+var jump_force = -300.0  # 跳跃力
+
 
 func enter() -> void:
 	pass
 
 func physics_process_update(delta: float) -> void:	
-	pass
+	snake.velocity.y = jump_force
+	
+	state_machine.change_state("Move")
 	
 
 ## 退出状态
