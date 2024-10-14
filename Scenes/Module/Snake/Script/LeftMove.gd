2291d72400a544e764e5b9bbb2a680a4aef603e1
aidewn <50902442+aidewn@users.noreply.github.com>
2024-10-14 20:43:51 +0800

    添加物理层级，添加蛇的跳跃和攻击状态

--- Scenes/Module/Snake/Script/LeftMove.gd
+++ Scenes/Module/Snake/Script/LeftMove.gd
@@ -3,6 +3,8 @@ extends StateBase
 @export var snake: Snake
 @onready var detect_the_player: RayCast2D = $"../../DetectThePlayer"
 @onready var ground_detection: RayCast2D = $"../../GroundDetection"
+@onready var animated_sprite_2d: AnimatedSprite2D = $"../../AnimatedSprite2D"
+@onready var attack_detection: RayCast2D = $"../../AnimatedSprite2D/AttackDetection"
 
 
 var player:Player
@@ -11,7 +13,6 @@ var is_on_floor = false  # 检测敌人是否在地面
 var speed:int = 100
 var gravity = 500.0 # 模拟重力
 var direction:int = 1
-
 
 func enter() -> void:
 	player = get_tree().root.find_child("Player",true,false)
@@ -22,23 +23,25 @@ func physics_process_update(delta: float) -> void:
 	if player:
 		if direction != sign(player.position.x - snake.position.x):
 			filp()
-		
 		set_velocity(delta)
-	
-	snake.move_and_slide()
-
+		
+		if attack_detection.get_collider() is Player:
+			state_machine.change_state("Attack")
+		
+		
 ## 退出状态
 func exit() -> void:
 	pass
 
 #翻转
 func filp() -> void:
-	snake.scale.x = -snake.scale.x
+	animated_sprite_2d.scale.x = -animated_sprite_2d.scale.x
 
 
 # 设置移动变量	
 func set_velocity(delta:float) -> void:
 	direction = sign(player.position.x - snake.position.x)
+	print(direction)
 	snake.velocity.x = direction * speed
 	snake.velocity.y += gravity * delta
 	
