35b5dc57975a454c8d31272c69b010cc50039ba3
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2024-10-15 19:09:50 +0800

    添加青蛙的翻滚，并添加翻滚音效

--- Scenes/Player/attack_2.gd
+++ Scenes/Player/attack_2.gd
@@ -12,15 +12,16 @@ func physics_process_update(delta: float) -> void:
 	if not player.is_on_floor():
 		state_machine.change_state("Fall")
 		
-	check_frog_collision(player, player.right, 40)
-	check_frog_collision(player, player.left, -40)
+	check_frog_collision(player, player.right, Vector2(120,0))
+	check_frog_collision(player, player.left, Vector2(-120,0))
 
 ## 退出状态
 func exit() -> void:
 	pass
 	
-func check_frog_collision(player: Player, ray: RayCast2D, offset: float) -> void:
+func check_frog_collision(player: Player, ray: RayCast2D, force:Vector2) -> void:
 	if ray.is_colliding() and ray.get_collider().name == "Frog":
 		if player.hand.animation == "Attack2" and player.hand.frame == 2:
-			var frog = ray.get_collider()
-			frog.position.x += offset
+			var frog:RigidBody2D = ray.get_collider()
+			frog.apply_central_impulse(force)
+			frog.sound_play()
--- Scenes/RigidFrog/frog.gd
+++ Scenes/RigidFrog/frog.gd
@@ -1,42 +1,10 @@
 extends RigidBody2D
 
-var player:CharacterBody2D
-var hand:AnimatedSprite2D
-var impulse:Vector2=Vector2(100,0)
-var current_degrees:float
-
 const SCRAPE = preload("res://Asset/Sounds/Level1/scrape.MP3")
-const FROG = preload("res://Asset/Sounds/Level1/frog.mp3")
 
 func _ready() -> void:
-	set_process(false)
-	#从全局组里获得玩家
-	player=get_tree().get_first_node_in_group("player")
-	#获得动画
-	hand=player.get_node("Body/Hand")
+	pass
 
-func _process(_delta: float) -> void:
-	#判断检测物是否为玩家
-
-	if $Left.is_colliding() && $Left.get_collider().name=="Player":
-		#若玩家动画为攻击二的第二帧则移动
-		if hand.animation=="Atack2" && hand.frame==2:
-			apply_central_impulse(impulse)
-			AudioPlayer.play(SCRAPE,false,true,-6)
-			
-	#判断检测物是否为玩家
-	if $Right.is_colliding() && $Right.get_collider().name=="Player":
-		#若玩家动画为攻击二的第二帧则移动
-		if hand.animation=="Atack2" && hand.frame==5:
-			apply_central_impulse(-impulse)
-			AudioPlayer.play(SCRAPE,false,true,-6)
-	
-	#变换冲量
-	if abs(self.rotation_degrees)-abs(current_degrees)>50:
-		if abs(self.rotation_degrees)>170:
-			impulse=Vector2(100,0)
-			self.rotation_degrees=current_degrees
-		elif abs(self.rotation_degrees)<10:
-			impulse=-Vector2(100,0)
-			self.rotation_degrees=current_degrees
-		
+#播放完音频自动销毁
+func sound_play()->void:
+	AudioPlayer.play(SCRAPE,false,true,7)
