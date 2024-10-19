0dfbe31cb581cad47205efbc360adf0d8e690a65
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2024-10-19 18:04:34 +0800

    再次修复碰撞

--- Scenes/RigidFrog/frog.gd
+++ Scenes/RigidFrog/frog.gd
@@ -6,7 +6,7 @@ const SCRAPE = preload("res://Asset/Sounds/Level1/scrape.MP3")
 var level1_shoot:bool=true
 
 @onready var player:CharacterBody2D=get_tree().get_first_node_in_group("player")
-@onready var death_ui:Control=get_tree().get_first_node_in_group("death_ui")
+
 
 @export var init_position:Vector2=Vector2(1447,939)
 
@@ -14,7 +14,7 @@ func _physics_process(delta: float) -> void:
 	if level1:
 		if self.global_position.x<200:
 			player.to_death=true
-			death_ui.get_node("TextureRect/YouDie").text="青蛙罢工了"
+			#death_ui.get_node("TextureRect/YouDie").text="青蛙罢工了"
 		if self.global_position.x>2300 and level1_shoot:
 			%SunRayManager.can_shoot=true
 			level1_shoot=false
