af86433c9d2de5a452f3442eb7cb71751839f3d0
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2024-10-25 09:50:10 +0800

    为第二关添加背景音乐

--- Scenes/Level2/level_2.gd
+++ Scenes/Level2/level_2.gd
@@ -2,21 +2,18 @@ extends Node2D
 
 @onready var tile_map_layer: TileMapLayer = $Envir/TileMapLayer
 #@onready var data:GameData=ResourceLoader.load("user://game_data.tres") as GameData
-
+const BG = preload("res://Asset/Sounds/Level2/Level - 2_Whispers in the Night.mp3")
 var is_light:bool
 var tile_top:Vector2=Vector2(0,0)
 
 func _ready() -> void:
-<<<<<<< Updated upstream
+	AudioPlayer.play(BG,true,false)
 	#if data:
 		#data.level="level2"
 	#ResourceSaver.save(data,"user://game_data.tres")
-=======
-	if data:
-		data.level="level2"
-	ResourceSaver.save(data,"user://game_data.tres")
+
 	pass
-	
+
 func _process(_delta: float) -> void:
->>>>>>> Stashed changes
+
 	pass
