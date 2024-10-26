29b8726e84cdbb442a9fba1124f53a12632f6745
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2024-10-26 15:36:09 +0800

    到达胜利火炬后无法移动

--- Scenes/Level3/sucess_area.gd
+++ Scenes/Level3/sucess_area.gd
@@ -4,6 +4,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
+		body.acceleration=0
 		var t:Tween=get_tree().create_tween()
 		t.tween_property(%SucessBg,"self_modulate:a",1,4)
 	
