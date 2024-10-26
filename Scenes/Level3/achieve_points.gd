408cfa2836a42cdacd1f3d6f80d676112cfcd936
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2024-10-26 15:23:56 +0800

    第三关存档基本完成

---
+++ Scenes/Level3/achieve_points.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+@onready var player:CharacterBody2D=get_tree().get_first_node_in_group("player")
+@onready var data=ResourceLoader.load("user://game_data.tres")
+
+
+func _on_achieve_area_1_body_entered(body: Node2D) -> void:
+	if body is Player:
+		data.achieve_point["player_position"]=self.position
+		data.achieve_point["player_body_scale"]=player.get_node("Body").scale.x
+		ResourceSaver.save(data,"user://game_data.tres")
+
+
+func _on_achieve_area_2_body_entered(body: Node2D) -> void:
+	if body is Player:
+		data.achieve_point["player_position"]=self.position
+		data.achieve_point["player_body_scale"]=player.get_node("Body").scale.x
+		ResourceSaver.save(data,"user://game_data.tres")
+
+
+func _on_achieve_area_3_body_entered(body: Node2D) -> void:
+	if body is Player:
+		data.achieve_point["player_position"]=self.position
+		data.achieve_point["player_body_scale"]=player.get_node("Body").scale.x
+		ResourceSaver.save(data,"user://game_data.tres")
+
+
+func _on_achieve_area_4_body_entered(body: Node2D) -> void:
+	if body is Player:
+		data.achieve_point["player_position"]=self.position
+		data.achieve_point["player_body_scale"]=player.get_node("Body").scale.x
+		ResourceSaver.save(data,"user://game_data.tres")
+
+
+func _on_achieve_area_5_body_entered(body: Node2D) -> void:
+	if body is Player:
+		data.achieve_point["player_position"]=self.position
+		data.achieve_point["player_body_scale"]=player.get_node("Body").scale.x
+		ResourceSaver.save(data,"user://game_data.tres")
